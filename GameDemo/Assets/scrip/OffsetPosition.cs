7c21e0493f0e1a269483f5daa4436bc155d61189
NhuLe <nhulemis@gmail.com>
2020-02-05 09:53:09 +0700

    + Add sources game

---
+++ GameDemo/Assets/scrip/OffsetPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetPosition : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_target)
+        {
+            var pos = new Vector3(m_target.transform.position.x, transform.position.y, transform.position.z);
+            transform.position = pos;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
